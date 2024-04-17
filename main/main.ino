6f4bf4ca363a3d6614842775325ee682fbdbdae6
mrfroggyjoe <mrfroggyjoe@gmail.com>
2024-04-17 12:04:00 -0700

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+// CPE FINAL PROJECT
+//Sofia
+//Joe
+//Helen
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+// Functions
+
