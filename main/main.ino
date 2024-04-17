a32613367befe3225403e340a31cfbb9daba5ae6
1103-Ahlstedt-Sofia <113205197+1103-Ahlstedt-Sofia@users.noreply.github.com>
2024-04-17 12:45:24 -0700

    Update main.ino
    water level function, adc functions

--- main/main.ino
+++ main/main.ino
@@ -11,6 +11,9 @@
  volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
  volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
+int waterLevel = 0;
+int threshold = 500;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,6 +30,14 @@ void loop() {
 //return 1 if okay 0 if too low
 int checkWaterLevel(){
 
+  pinMode(waterLevelPin, OUTPUT);
+  waterLevel = adc_read(0);
+
+ if(waterLevel >= threshold){
+}
+ else if(waterLevel< threshold){
+  lcd.print("Water level too low");
+}
 }
 
 // the movement up or down
@@ -36,9 +47,7 @@ void moveVent(int direction){
 
 // display message via LCD
 void LCDDisplay(){
-lcd.clear(); // Clear the LCD display
-lcd.setCursor(0, 0); // Set the cursor to the top-left position
-lcd.print(number); // Print the number to the LCD
+
 }
 
 // display message via UART/Serial monitor
@@ -74,4 +83,46 @@ void UARTStart(unsigned long baud){
 void UARTOut(unsigned char out){
   while(!(*myUCSR0A & 0b00100000)){}
   *myUDR0 = out;
+}
+
+
+//ADC functions:
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
 }
